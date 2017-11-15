5eb1c9d745f15c686dfde4cfe3e8939e58034c1f
Bal <bal200@gmail.com>
2017-11-15 20:17:45 +0000

    some more clouds on the title screen.

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,4 @@
 
-
 var menuState = {
   preload: function() {
     myGame = this;
@@ -8,14 +7,14 @@ var menuState = {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     /* if the full screen button is pressed, use this scale mode: */
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+    game.load.image("fullscreenbutton", "img/fullscreenbutton.png");
     game.load.image("ribbon", "img/ribbon700w.png");
     game.load.image("biplane", "img/biplane550w.png");
     game.load.image("pilot", "img/aviator125w.png");
     game.load.image("sky", "img/sky720h.jpg");
     game.load.spritesheet("buttons", "img/buttons103w47h.png", 103,47);
     game.load.spritesheet("smoke", "img/smoke200wh.png", 200,200);
-   
-
   },
   create: function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -23,7 +22,10 @@ var menuState = {
 
     //game.stage.backgroundColor = '#94e1f3'; /* sky blue */
     game.add.tileSprite(0,0, 1280, 720, 'sky');
-    game.world.setBounds(-5,-5, game.width+10,game.height+10);
+    //game.world.setBounds(-5,-5, game.width+10,game.height+10);
+
+    this.clouds = new Clouds( game.world );
+    this.clouds.createClouds( 15 );
 
     this.ribbon = game.add.sprite(game.width/2, game.height/2, 'ribbon');
     this.ribbon.anchor.set(0.5, 0.5); this.ribbon.scale.set(0.85,0.85);
@@ -49,6 +51,7 @@ var menuState = {
 
     this.combineWobbles();
 
+    this.fullScreenButton = game.add.button(3,3, 'fullscreenbutton', this.fullScreenButtonPress, this,0,0,0);    
   },
   update: function() {
     this.count++;
@@ -61,23 +64,9 @@ var menuState = {
       this.inTheClouds.createClouds( game.rnd.between(2,4) );
     }
 
-    if (this.gameMode==GAME) {
-      if (this.cursors.up.isDown) {
-        this.player.plane.accelerate(ACCELERATE_SPEED);
-      }
-      else if (this.cursors.down.isDown) {
-        this.player.plane.decelerate(ACCELERATE_SPEED);
-      }
-      if (this.cursors.left.isDown) {
-        this.player.plane.rotate(-TURN_SPEED);
-      }
-      else if (this.cursors.right.isDown) {
-        this.player.plane.rotate(+TURN_SPEED);
-      }
-      if (this.spacebar.isDown) {
-        this.fireButtonClick();
-      }
-    }
+    this.clouds.update();
+
+
   },
   combineWobbles: function() {
     this.plane.x=0; this.plane.y=0;
@@ -169,7 +158,7 @@ InTheClouds.prototype.createClouds = function ( count ) {
     if (cld=this.getFirstExists(false)) {
       cld.reset(game.width + cloudSize/2, game.height/2);
       cld.anchor.set(0.5,0.5);
-      cld.frame = game.rnd.between(1,5);
+      cld.frame = game.rnd.between(3,5);
       cld.alpha=alpha;
       cld.scale.set(cloudScale, cloudScale);
       dist += (game.rnd.between(1,3)==1 ? 90 : 0);
@@ -188,8 +177,9 @@ InTheClouds.prototype.createClouds = function ( count ) {
 var Clouds = function ( group ) {
   Phaser.Group.call(this, game); /* create a Group, the parent Class */
   
-  this.createMultiple(5, 'cloud'/*sprite sheet*/);
+  this.createMultiple(20, 'smoke'/*sprite sheet*/);
   group.add( this );
+  game.physics.enable(this, Phaser.Physics.ARCADE);
   this.forEach(function(cld) {
     cld.anchor.set(0.5, 0.5);
   });
@@ -200,22 +190,19 @@ Clouds.prototype.constructor = Clouds;
 
 /* create a big explosion graphic */
 Clouds.prototype.createClouds = function ( count ) {
-  var cld;
-  var level=1;
-  for (var n=0; n<10; n++) { /* Level 1 */
-    cloudScale = 1.0;
+  var cld, lev;
+  for (var n=0; n<count; n++) { 
+    lev = game.rnd.between(1,2); /* lev 1: near+large+fast. lev 2: far+small+slow */
+    cloudScale = (lev==1 ? 2.0 : 1.0);
+    cloudSpeed = (lev==1 ? 30 : 10);
     if (cld=this.getFirstExists(false)) {
       cld.reset(game.rnd.between(0,game.width), game.rnd.between(0,game.height));
       cld.anchor.set(0.5,0.5);
-      cld.frame = game.rnd.between(1,2);
+      cld.frame = game.rnd.between(1,5);
       cld.alpha=0.9;
+      cld.angle=game.rnd.between(0, 360);
       cld.scale.set(cloudScale, cloudScale);
-      dist += (game.rnd.between(1,3)==1 ? 90 : 0);
-      game.add.tween(cld).to({x: -(cloudSize/2)}, /*duration*/300, Phaser.Easing.Linear.None,
-        /*autostart*/true, /*delay*/dist, /*repeat*/0, /*yoyo*/false)
-        .onComplete.add(function(cld, tween){
-          cld.kill();
-        });
+      cld.body.velocity = new Phaser.Point(-cloudSpeed, 0);
 
     }
   }
@@ -224,6 +211,8 @@ Clouds.prototype.createClouds = function ( count ) {
 
 Clouds.prototype.update = function () {
   this.forEach(function(cld) {
-    
+    if (cld.x < -200) {cld.x = game.width+200; cld.y=game.rnd.between(0,game.height);}
+    if (cld.x > game.width+200) {cld.x = -200; cld.y=game.rnd.between(0,game.height);}
   });
+
 };
